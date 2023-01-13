bb30c74364218cd2b45d57527188d37b0efe9c14
Lamia Bdj <55047182+Lam-IA@users.noreply.github.com>
2023-01-13 13:12:56 +0100

    Update TestLogin.java

--- src/test/java/org/qaway/TestLogin.java
+++ src/test/java/org/qaway/TestLogin.java
@@ -1,10 +1,5 @@
 package org.qaway;
 
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebElement;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.qaway.base.CommonAPI;
