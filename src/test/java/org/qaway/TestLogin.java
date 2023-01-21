8464e9f3d7282e8a8e262bb5e4f4fe32113b171c
Lamia Bdj <55047182+Lam-IA@users.noreply.github.com>
2023-01-21 12:29:18 +0100

    Update TestLogin.java

--- src/test/java/org/qaway/TestLogin.java
+++ src/test/java/org/qaway/TestLogin.java
@@ -22,235 +22,69 @@ public class TestLogin extends CommonAPI { // je rajoute extends CommonAPI
     public TestLogin() throws ClassNotFoundException {
     }
 
-
-    // on va créer un autre test
     //@Test // il est connectée à la base de données
     public void loginWithValidCredential() {    //j'élimine "throws InterruptedException"
         LoginPage loginPage = new LoginPage(driver); // faut le mettre dans la méthode sinon ça ne va pas marcher
         HomePage homePage = new HomePage(driver); // juste pour cette méthode
    
         String expected = excelReader.getDataForGivenHeaderAndKey("key", "login page title");
-//        String actual = driver.getTitle();
         String actual = getPageTitle();
         Assert.assertEquals(expected, actual);
-//        System.out.println("page title validation succes");
         LOG.info("page title validation succes");
-//        Thread.sleep(3000);
-//        waitFor(3);
 
         // enter username
-//        WebElement usernameFiled=driver.findElement(By.id("user-name"));
-//        usernameFiled.sendKeys("standard_user");// envoyer une certaine formation
-        // j'utilise l'objet loginPage pour appeler la méthode type
-//        loginPage.typeUsername(username);
-        //loginPage.typeUsername(dbUsername);
-//        loginPage.typeUsername("standard_user");
-//        type("#user-name", "standard_user");
-        // boolean userNameIsDisplayed = usernameFiled.isDisplayed(); on enleve les validations
-        // Assert.assertTrue(userNameIsDisplayed); // on va l'enlever cette étape car on ne va pas décider si elle est cindée ou pas
         System.out.println("enter username success");
-//        Thread.sleep(3000);
-//        waitFor(3);
+
 
         // enter password
-//        WebElement passwordFiled=driver.findElement(By.id("password"));
-//        passwordFiled.sendKeys("secret_sauce");
-        // j'utilise l'objet loginPage pour appeler la méthode type
-//        loginPage.typePassword(password);
-        //loginPage.typePassword(dbPassword);
-//        type("#password", "secret_sauce");
-        // boolean passwordIsDisplayed = usernameFiled.isDisplayed();
-        // Assert.assertTrue(passwordIsDisplayed);
+
         System.out.println("enter password success");
-//        Thread.sleep(3000);
-//        waitFor(3);
 
         // click login button
-//        WebElement loginButtonFiled=driver.findElement(By.id("login-button"));
-//        loginButtonFiled.click();
-        // j'utilise l'objet loginPage pour appeler la méthode type
         loginPage.clickOnLoginButton();
-//        clickOn("#login-button"); // ne prend plus des strings plutot des WebElement
-        // boolean loginButtonIsDisplayed = usernameFiled.isDisplayed();
         System.out.println("login button click succes");
-        // je clique sur le boutton "LOGIN" et je vérifie si ça marche ou pas
-//        WebElement productsHeader = driver.findElement(By.cssSelector(".title")); // on récupere le css selector
-//        boolean productsHeaderIsDisplayed = productsHeader.isDisplayed();
-//        boolean productsHeaderIsDisplayed = isDisplayed(".title");
-        boolean productsHeaderIsDisplayed = homePage.productsHeaderIsDisplayed(); // productsHeader n'est pas dans la page,
-        // donc on va créer une nouvelle classe HomePage
-        Assert.assertTrue(productsHeaderIsDisplayed); // par contre cette étape nécéssite une validation
-//        System.out.println("Products header is displayed success");
+        boolean productsHeaderIsDisplayed = homePage.productsHeaderIsDisplayed(); 
+        Assert.assertTrue(productsHeaderIsDisplayed); 
         LOG.info("Products header is displayed success");
-//        Thread.sleep(3000);
-//        waitFor(3);
     }
 
 
     @Test
     public void loginAttemptWithoutUsername() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
-        // check user land and the right page
-//        String expected = "Swag Labs";
         String expected = excelReader.getDataForGivenHeaderAndKey("key", "login page title");
-//        String actual = driver.getTitle();
         String actual = getPageTitle();
         Assert.assertEquals(expected, actual);
-//        System.out.println("page title validation succes");
         LOG.info("page title validation succes");
-//        Thread.sleep(3000);
-//        waitFor(3);
-
 
         // enter username
-//        WebElement usernameFiled=driver.findElement(By.id("user-name"));
-//        usernameFiled.sendKeys("standard_user");// envoyer une certaine formation
-        // j'utilise l'objet loginPage pour appeler la méthode type
         loginPage.typeUsername("");
-//        type("#user-name", "");
-        // boolean userNameIsDisplayed = usernameFiled.isDisplayed(); on enleve les validations
-        // Assert.assertTrue(userNameIsDisplayed); // on va l'enlever cette étape car on ne va pas décider si elle est cindée ou pas
-//        System.out.println("enter username success");
-//        Thread.sleep(3000);
-//        waitFor(3);
 
-        // enter password
-//        WebElement passwordFiled=driver.findElement(By.id("password"));
-//        passwordFiled.sendKeys("secret_sauce");
-
-//        type("#password", "secret_sauce");
-        // boolean passwordIsDisplayed = usernameFiled.isDisplayed();
-        // Assert.assertTrue(passwordIsDisplayed);
-//        System.out.println("enter password success");
-//        Thread.sleep(3000);
-//        waitFor(3);
-
-//        // enter username
-//        WebElement usernameFiled=driver.findElement(By.id("user-name"));
-//        usernameFiled.sendKeys("");// envoyer une certaine formation
-//        // boolean userNameIsDisplayed = usernameFiled.isDisplayed(); on enleve les validations
-//        // Assert.assertTrue(userNameIsDisplayed); // on va l'enlever cette étape car on ne va pas décider si elle est cindé ou pas
-//        System.out.println("enter username success");
-//        Thread.sleep(3000);
-//
-//        // enter password
-//        WebElement passwordFiled=driver.findElement(By.id("password"));
-//        passwordFiled.sendKeys("secret_sauce");
-//        // boolean passwordIsDisplayed = usernameFiled.isDisplayed();
-//        // Assert.assertTrue(passwordIsDisplayed);
-//        System.out.println("enter password success");
-//        Thread.sleep(3000);
-
-        // click login button
-//        WebElement loginButtonFiled=driver.findElement(By.id("login-button"));
-//        loginButtonFiled.click();
-        // j'utilise l'objet loginPage pour appeler la méthode type
         loginPage.typePassword(password);
-//        clickOn("#login-button");
-        // boolean loginButtonIsDisplayed = usernameFiled.isDisplayed();
         loginPage.clickOnLoginButton();
-//        System.out.println("login button click succes");
         LOG.info("login button click succes");
-        // je clique sur le boutton "LOGIN" et je vérifie si ça marche ou pas
-//        WebElement errorAlert = driver.findElement(By.cssSelector(".error-message-container.error")); // on récupere le css selector
-        // boolean productsHeaderIsDisplayed = productsHeader.isDisplayed(); // il ne s'agit pas de vérifier
-//        String textError = errorAlert.getText();// je mets le text dans un string
-//        String textError = getWebElementText(".error-message-container.error");
-        //on remplace le dessus ainsi:
         String textError = loginPage.getErrorMessage();
-//        System.out.println("error message: "+ textError);
         LOG.info("error message: "+ textError);
-//        Assert.assertEquals("Epic sadface: Username is required",textError);
         Assert.assertEquals(excelReader.getDataForGivenHeaderAndKey("key", "invalid username error message"), textError);
-//        System.out.println("error message validation succes");
         LOG.info("error message validation succes");
-        //Assert.assertTrue(productsHeaderIsDisplayed); // par contre cette étape nécéssite une validation
-//        Thread.sleep(3000);
-//        waitFor(3);
     }
 
     @Test
     public void  loginAttemptWithoutPassword() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
-        // check user land and the right page
-//        String expected = "Swag Labs";
         String expected = excelReader.getDataForGivenHeaderAndKey("key", "login page title");
-//        String actual = driver.getTitle();
         String actual = getPageTitle();
         Assert.assertEquals(expected, actual);
-//        System.out.println("page title validation succes");
         LOG.info("page title validation succes");
-//        Thread.sleep(3000);
-//        waitFor(3);
 
-        // enter username
-//        WebElement usernameFiled=driver.findElement(By.id("user-name"));
-//        usernameFiled.sendKeys("standard_user");// envoyer une certaine formation
-        // j'utilise l'objet loginPage pour appeler la méthode type
         loginPage.typeUsername(username);
-//        type("#user-name", "standard_user");
-        // boolean userNameIsDisplayed = usernameFiled.isDisplayed(); on enleve les validations
-        // Assert.assertTrue(userNameIsDisplayed); // on va l'enlever cette étape car on ne va pas décider si elle est cindée ou pas
-//        System.out.println("enter username success");
-//        Thread.sleep(3000);
-//        waitFor(3);
-        // enter password
-//        WebElement passwordFiled=driver.findElement(By.id("password"));
-//        passwordFiled.sendKeys("secret_sauce");
-        // j'utilise l'objet loginPage pour appeler la méthode type
         loginPage.typePassword("");
-//        type("#password", "");
-        // boolean passwordIsDisplayed = usernameFiled.isDisplayed();
-        // Assert.assertTrue(passwordIsDisplayed);
-//        System.out.println("enter password success");
-//
-//        waitFor(3);
-//        // enter username
-//        WebElement usernameFiled=driver.findElement(By.id("user-name"));
-//        usernameFiled.sendKeys("standard_user");// envoyer une certaine formation
-//        // boolean userNameIsDisplayed = usernameFiled.isDisplayed(); on enleve les validations
-//        // Assert.assertTrue(userNameIsDisplayed); // on va l'enlever cette étape car on ne va pas décider si elle est cindé ou pas
-//        System.out.println("enter username success");
-//        Thread.sleep(3000);
-//
-//        // enter password
-//        WebElement passwordFiled=driver.findElement(By.id("password"));
-//        passwordFiled.sendKeys("");
-//        // boolean passwordIsDisplayed = usernameFiled.isDisplayed();
-//        // Assert.assertTrue(passwordIsDisplayed);
-//        System.out.println("enter password success");
-//        Thread.sleep(3000);
-
-        // click login button
-//        WebElement loginButtonFiled=driver.findElement(By.id("login-button"));
-//        loginButtonFiled.click();
-        // j'utilise l'objet loginPage pour appeler la méthode type
-//        clickOn("#login-button");
-        // boolean loginButtonIsDisplayed = usernameFiled.isDisplayed();
         loginPage.clickOnLoginButton();
-//        System.out.println("login button click succes");
         LOG.info("login button click succes");
-        // je clique sur le boutton "LOGIN" et je vérifie si ça marche ou pas
-//        WebElement errorAlert = driver.findElement(By.cssSelector(".error-message-container.error")); // on récupere le css selector
-        // boolean productsHeaderIsDisplayed = productsHeader.isDisplayed(); // il ne s'agit pas de vérifier
-//        String textError = errorAlert.getText();// je mets le text dans un string
-        //on remplace le dessus ainsi:
-        String textError = loginPage.getErrorMessage();
-//        String textError = getWebElementText(".error-message-container.error");
-//        System.out.println("error message: "+ textError);
+        String textError = loginPage.getErrorMessage();;
         LOG.info("error message: "+ textError);
-//        Assert.assertEquals("Epic sadface: Password is required",textError);
         Assert.assertEquals(excelReader.getDataForGivenHeaderAndKey("key", "invalid password error message"), textError);
-//        System.out.println("error message validation succes");
         LOG.info("error message validation succes");
-        //Assert.assertTrue(productsHeaderIsDisplayed); // par contre cette étape nécéssite une validation
-//        Thread.sleep(3000);
-//        waitFor(3);
     }
 }
 
-
-// Remarque: quand on a modifié le System.out.println par LOG.info et q'uon a compilé ce test on a pas eu
-// de log, on est obligé de rajouter le ficher log4j2.xml dans ressources pour que ça m'affiche ces log
-// grace à <AppenderRef ref="Console"/>
-// on peut meme les mettre dans un fichier    <AppenderRef ref="file"/>
